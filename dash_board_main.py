92c2ee18f7e3f13f9a8ea1b87837da877f0fe7ee
abubakar12 <abubakarilyas624@gmail.com>
2022-07-22 19:17:19 +0500

    separate  files

--- dash_board_main.py
+++ dash_board_main.py
@@ -3,106 +3,53 @@ import dash_bootstrap_components as dbc
 import numpy as np
 from dash_extensions.enrich import DashProxy, Output, Input, State, ServersideOutput, html, dcc, \
     ServersideOutputTransform,callback,FileSystemStore,callback_context,MATCH, ALL
+selected_chart_template='simple_white'
+import plotly.graph_objects as go
 
-class dashboard:
-    def __init__(self, df, user_style,kpi_small="14rem",kpi_big="18rem"):
-        self.df = df
-        self.user_style = user_style
-        self.kpi_small=kpi_small
-        self.kpi_big=kpi_big
+sample_file=pd.read_csv(r"C:\Users\hp\Desktop\Shopify_app\Shopify_next_level\sample_file.csv")
+sample_file=sample_file.drop_duplicates(subset=["TYear","TMonth"])
+sample_file["Date"]=pd.to_datetime(sample_file["TYear"].astype(str)+"-"\
+                                   +sample_file["TMonth"].astype(str),format="%Y-%m")
+    
+from dashboarding import dashboard_class
 
 
-    def bordered_table(self):
-        table = dbc.Table.from_dataframe(self.df, striped=True, bordered=True, hover=True,style=self.user_style)
-        return table
     
     
-    def simple_table(self):
-        table = dbc.Table.from_dataframe(self.df, striped=True, bordered=False, hover=True,style=self.user_style)
-        return table
-    
-    def card_bigger_kpi(self,header="",card_title="",paragraph="",footer="",):
-        card = dbc.Card(
-        [
-            dbc.CardHeader(header),
-            dbc.CardBody(
-                [
-                    html.H4(card_title, className="card-title"),
-                    html.P(paragraph, className="card-text"),
-                ]
-            ),
-            dbc.CardFooter(footer),
-        ],
-        style={"width": self.kpi_big},
-                        )
-        
-        return card
-    
-    
-    def card_smaller_kpi(self,position,header="",card_title="",paragraph="",footer=""):
-        card = dbc.Card(
-
-        [
-            dbc.CardHeader(header),
-            dbc.CardBody(
-                [
-                    html.H4(card_title, className="card-title"),
-                    html.P(paragraph, className="card-text"),
-                ]
-            ),
-            dbc.CardFooter(footer),
-        ],
-        style={"width": self.kpi_small},
-        id={
-                'type': 'small-kpi',
-                'index': position
-            },
-                        )
-        
-        return card
-    
-    def kpi_creator(self,total,columns):
-        rows=int(total/columns)
-        if rows<1:
-            rows=1
-        position=0
-        
-        card_row=[]
-        for row in range(rows):
-            card_col=[]
-            for column in range(columns):
-                
-                card=dbc.Col(self.card_smaller_kpi(position))
-                position=position+1
-                card_col.append(card)
-            card_row.append(dbc.Row(card_col,className="g-0"))
-        
-        return card_row
             
-            
-                
+ 
+
+               
 header = ['COGS','Discounts','Shipping Cost','Shipping Charged','Avg.Order Value','Avg Order Profit','LTV','CAC','Net Margin',\
           'Gross Margin','Ad Spend','Ad Speed/Order','Handling Fee','Transaction Fee','Disputed Revenue','Refunded',\
               'Unique Visitors','Conversion Rate','Revenue Visitor','Net Profit/Visitor']
 card_title=[]
 
+header_big = ['Number Of Orders','Number Of Refunds and Cancellations','Unique Visitors','Conversion Rate']
+card_title=[]
+
             
-            
-    
 
+first_page=dashboard_class.dashboard(sample_file,user_style={"text-align": "auto",
+                  'max-height': '200px',
+                  'overflow': 'auto'})  
 
+layout1 = html.Div([
+    dbc.Spinner(dcc.Graph(id="graph1"),color="info")
 
-    
-    
-
-first_page=dashboard("","")
-
+])  
                             
 main_page_var = dbc.Container(
     [   
-        
+        dbc.Spinner(html.Div(dbc.Table(id='simple_table'),style={"overflow": "scroll"})),
+        dbc.Spinner(html.Div(dbc.Table(id='bordered_table',style={
+                          'max-height': '200px',
+                          'overflow': 'auto'}),style={"overflow": "scroll"})),
+        html.Div(id="check_list"), 
+        layout1,
         html.Hr(),
-        dbc.Container(first_page.kpi_creator(16,4))
+        dbc.Container(first_page.kpi_creator_smaller(16,4)),
+        dbc.Container(first_page.kpi_creator_bigger(4,2)),
     ],
 )
 
@@ -124,13 +71,18 @@ dash_app.layout = dbc.Container(
    fluid=True
 )
 
+
+##############################################################################
 @dash_app.callback(
     Output({'type': 'small-kpi', 'index': ALL}, 'children'),
     Input("url", "pathname"),
     State({'type': 'small-kpi', 'index': ALL}, 'value')
 )
-def display_output(values,val):
-    first_page=dashboard("","")
+def fill_kpi_values_smaller(values,val):
+    
+    
+
+    first_page=dashboard_class.dashboard(sample_file,user_style={"text-align": "center"})
     tot_position=range(16)
     card_title=np.repeat('$0',16)
     
@@ -138,11 +90,69 @@ def display_output(values,val):
     return [first_page.card_smaller_kpi(position=pos,header=head,card_title=card_tit) \
             for head,pos,card_tit in zip(header,tot_position,card_title)
             ]
+        
+        
+@dash_app.callback(
+    Output({'type': 'big-kpi', 'index': ALL}, 'children'),
+    Input("url", "pathname"),
+    State({'type': 'big-kpi', 'index': ALL}, 'value')
+)
+def fill_kpi_values_bigger(values,val):
+    
+    first_page=dashboard_class.dashboard(sample_file,user_style={"text-align": "center"})
+    tot_position=range(4)
+    card_title=np.repeat('$0',4)
     
 
+    return [first_page.card_bigger_kpi(position=pos,header=head,card_title=card_tit) \
+            for head,pos,card_tit in zip(header_big,tot_position,card_title)
+            ]
+        
+        
+        
+@dash_app.callback(
+    Output('simple_table','children'),
+    Input("url", "pathname"),
+)
+def simple_table(val):
+    return first_page.simple_table()     
 
+    
+@dash_app.callback(
+    Output('bordered_table','children'),
+    Input("url", "pathname"),
+)
+def bordered_table(val):
+    return first_page.bordered_table()   
 
+    
+        
+##############################################################################        
+        
+     
+@dash_app.callback(
+    Output('graph1', 'figure'),
+    Input("check_list", "value"),
+)
+def fill_graph(val):
+    
+    return first_page.trace_graphs(val) 
+  
+   
+##############################################################################
+@dash_app.callback(
+    Output("check_list", 'children'),
+    Input("url", "pathname"),
+)
+def make_checkbox(path):
+        
+        return dcc.Checklist(
+                id='check_list',
+                options=['Actual Sales', 'Projected Sales'],
+                value=['Actual Sales']
+            ),
 
+###############################################################################
 # Multi-page selector callback - not really used, but left in for future use
 @dash_app.callback(Output("page-content", "children"), Input("url", "pathname"))
 def display_page(pathname):
---
+++ dashboarding/dashboard_class.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import dash_bootstrap_components as dbc
+import numpy as np
+from dash_extensions.enrich import DashProxy, Output, Input, State, ServersideOutput, html, dcc, \
+    ServersideOutputTransform,callback,FileSystemStore,callback_context,MATCH, ALL
+selected_chart_template='simple_white'
+import plotly.graph_objects as go
+
+
+class dashboard:
+    
+    def __init__(self, df, user_style={"overflow": "auto"},kpi_small="14rem",kpi_big="18rem"):
+        self.df = df
+        self.user_style = user_style
+        self.kpi_small=kpi_small
+        self.kpi_big=kpi_big
+
+
+    def bordered_table(self):
+        table = dbc.Table.from_dataframe(self.df, striped=True, bordered=True, hover=True,style=self.user_style)
+        return table
+    
+    
+    def simple_table(self):
+        table = dbc.Table.from_dataframe(self.df, striped=True, bordered=False, hover=True,style=self.user_style)
+        return table
+    
+    def card_bigger_kpi(self,position,header="",card_title="",paragraph="",footer="",):
+        card = dbc.Card(
+        [
+            dbc.CardHeader(header),
+            dbc.CardBody(
+                [
+                    html.H4(card_title, className="card-title"),
+                    html.P(paragraph, className="card-text"),
+                ]
+            ),
+            dbc.CardFooter(footer),
+        ],
+        style={"width": self.kpi_big},
+        id={
+                'type': 'big-kpi',
+                'index': position
+            },
+        
+                        )
+        
+        return card
+    
+    
+    def card_smaller_kpi(self,position,header="",card_title="",paragraph="",footer=""):
+        card = dbc.Card(
+
+        [
+            dbc.CardHeader(header),
+            dbc.CardBody(
+                [
+                    html.H4(card_title, className="card-title"),
+                    html.P(paragraph, className="card-text"),
+                ]
+            ),
+            dbc.CardFooter(footer),
+        ],
+        style={"width": self.kpi_small},
+        id={
+                'type': 'small-kpi',
+                'index': position
+            },
+                        )
+        
+        return card
+    
+    def kpi_creator_smaller(self,total,columns):
+        rows=int(total/columns)
+        if rows<1:
+            rows=1
+        position=0
+        
+        card_row=[]
+        for row in range(rows):
+            card_col=[]
+            for column in range(columns):
+                
+                card=dbc.Col(self.card_smaller_kpi(position))
+                position=position+1
+                card_col.append(card)
+            card_row.append(dbc.Row(card_col,className="g-0"))
+        
+        return card_row
+    
+    def kpi_creator_bigger(self,total,columns):
+        rows=int(total/columns)
+        if rows<1:
+            rows=1
+        position=0
+        
+        card_row=[]
+        for row in range(rows):
+            card_col=[]
+            for column in range(columns):
+                
+                card=dbc.Col(self.card_bigger_kpi(position))
+                position=position+1
+                card_col.append(card)
+            card_row.append(dbc.Row(card_col,className="g-0"))
+        
+        return card_row
+     
+    def trace_graphs(self,columns_used,name_date_columns="Date"):
+
+        df=self.df
+
+        
+        fig = go.Figure()
+        try:
+            for col in columns_used:
+                fig.add_trace(go.Scatter(x=df[name_date_columns], y=df[col],
+                                    mode='lines+markers',
+                                    name=col,customdata=df['Date'],
+                                    # marker_color=ai_green
+                                    ))
+        except:
+            for col in columns_used:
+                fig.add_trace(go.Scatter(x=df[name_date_columns], y=df[col],
+                                    mode='lines+markers',
+                                    name=col,customdata=df['Date'],
+                                    # marker_color=ai_green
+                                    ))
+            
+
+            
+        fig=fig.update_layout(template=selected_chart_template)
+        fig=fig.update_layout(margin={'t': 30, 'b': 0,'l':0,'r':0})
+        return fig   
